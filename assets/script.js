ed6b3eab597fec7ef218501cde6ddfb7412c3845
wayne <kuderge2002@mail.ru>
2025-03-03 01:11:07 +0300

    Added languages list

--- assets/script.js
+++ assets/script.js
@@ -13,15 +13,29 @@ $(document).ready(function() {
             }
 
             let output = '<ul>';
-            console.log('Ответ от API:', data);
+            console.log('Api answer:', data);
 
             data.forEach(repo => {
-                output += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`;
+                output += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a><br>
+                <strong>Description: </strong> ${repo.description || 'No description'} <br>
+                <strong>Created at: </strong> ${new Date(repo.created_at).toLocaleDateString()} <br>
+                <strong>Stars: </strong> ${repo.stargazers_count} <br>
+                <strong>Forks: </strong> ${repo.forks_count} <br>
+                <strong>Languages: </strong> <span id="languages-${repo.id}"> Wait a sec...</span> <br> 
+                </li>`;
+
+                $.getJSON(repo.languages_url, function (languages) {
+                    let languagesList = Object.keys(languages).join(', ') || 'No lang data';
+                    $(`#languages-${repo.id}`).text(languagesList);
+                }).fail(function() {
+                    $(`#languages-${repo.id}`).text('Failed to load lang data');
+                });
             });
+            
             output += '</ul>';
             $('#list-of-repos').html(output);
         }).fail(function() {
-            $('#list-of-repos').html(`<p>Ошибка AJAX: ${textStatus} - ${errorThrown}</p>`);
+            $('#list-of-repos').html(`<p>AJAX error: ${textStatus} - ${errorThrown}</p>`);
         });
     });
 })
